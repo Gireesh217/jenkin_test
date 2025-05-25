db479117f4337c5b9ccb555f3028347453eb07e8
Gireesh217 <gireesh.engineer1@gmail.com>
2025-05-25 16:42:20 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+// src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is a placeholder for a Java app.");
+    }
+}
